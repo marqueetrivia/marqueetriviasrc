9230b5efb94b15d7acc36041a0a8bc343562e4ac
jamacon36 <james@Jamess-MacBook-Pro.local>
2020-04-06 08:36:33 -0400

    updating april 2020

--- client/src/components/EventCard.vue
+++ client/src/components/EventCard.vue
@@ -1,46 +1,49 @@
 <template>
-  <div class="card">
+  <div :class="['card', event.node.slug]">
     <div class="event__inner grid">
       <div class="grid-1of3 grid-push-2of3 grid-1of1--tablet-large">
-        <g-link
-        :to='`/triviaevent/${event.node.slug}`'>
-          <div class="event__image background-image"
-          :style="{backgroundImage: `url(/triviaevents${event.node.acf.photoAsset})`}"/>
+        <g-link :to="`/triviaevent/${event.node.slug}`">
+          <div
+            class="event__image background-image"
+            :style="{
+              backgroundImage: `url(/triviaevents${event.node.acf.photoAsset})`
+            }"
+          />
         </g-link>
       </div>
       <div class="grid-2of3 grid-pull-1of3 grid-1of1--tablet-large">
         <div class="event__details">
-          <g-link
-          :to='`/triviaevent/${event.node.slug}`'>
-            <h3 class="event__title">{{event.node.title}}</h3>
-            <p
-            v-if="event.node.acf"
-            class="date">
-              {{eventDate(event)}}
+          <g-link :to="`/triviaevent/${event.node.slug}`">
+            <h3 class="event__title">{{ event.node.title }}</h3>
+            <p v-if="event.node.acf" class="date">
+              {{ eventDate(event) }}
             </p>
           </g-link>
           <a
-          :href='`https://maps.google.com/?q=${event.node.acf.location.address}`'
-          rel="no-openner"
-          target="_blank"
-          class="location">
-            <font-awesome-icon :icon="['fas', 'map-marked-alt']"/>
+            v-if="event.node.acf.location.address"
+            :href="
+              `https://maps.google.com/?q=${event.node.acf.location.address}`
+            "
+            rel="no-openner"
+            target="_blank"
+            class="location"
+          >
+            <font-awesome-icon :icon="['fas', 'map-marked-alt']" />
             White Horse Tavern
           </a>
-          <g-link
-          :to='`/triviaevent/${event.node.slug}`'>
-            <div
+          <g-link :to="`/triviaevent/${event.node.slug}`">
+            <!-- <div
             class="event__description"
-            v-html="event.node.acf.description"/>
+            v-html="event.node.acf.description"/> -->
           </g-link>
-          <div
-          v-if="isPresent"
-          class="event__ctas">
+          <div v-if="isPresent" class="event__ctas">
             <a
-            :href="event.node.acf.googleCalendarLink"
-            class="button"
-            target="_blank"
-            rel="noopenner">Add To Google Calendar</a>
+              :href="event.node.acf.googleCalendarLink"
+              class="button"
+              target="_blank"
+              rel="noopenner"
+              >Add To Google Calendar</a
+            >
           </div>
         </div>
       </div>
@@ -50,22 +53,43 @@
 
 <script>
 export default {
-  name: 'EventCard',
+  name: "EventCard",
   props: {
     event: Object,
-    isPresent: Boolean,
+    isPresent: Boolean
   },
   methods: {
     eventDate(event) {
       const months = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
       ];
-      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const eventDate = new Date(event.node.acf.dateTime.replace(/\-/g, '/'));
-      const eventTime = `${(eventDate.getHours() + 11) % 12 + 1}:${eventDate.getMinutes()}`;
-      return `${days[eventDate.getDay()]}, ${months[eventDate.getMonth()]} ${eventDate.getDate()} at ${eventTime}pm`;
-    },
-  },
-}
+      const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      const eventDate = new Date(event.node.acf.dateTime.replace(/\-/g, "/"));
+      const eventTime = `${((eventDate.getHours() + 11) % 12) +
+        1}:${eventDate.getMinutes()}`;
+      return `${days[eventDate.getDay()]}, ${
+        months[eventDate.getMonth()]
+      } ${eventDate.getDate()} at ${eventTime}pm`;
+    }
+  }
+};
 </script>
--- client/src/templates/WordPressTriviaEvent.vue
+++ client/src/templates/WordPressTriviaEvent.vue
@@ -1,44 +1,46 @@
 <template>
   <Layout>
-    <main class="trivia-event">
+    <main :class="['trivia-event', event.slug]">
       <header class="trivia-event__header">
         <div class="grid">
           <div class="grid-1of3 grid-1of1--notebook-large section section--red">
             <div class="trivia-event__header__title">
-              <h1>{{event.title}}</h1>
-              <p
-              v-if="event.acf"
-              class="date">
-                {{eventDate}}
+              <h1 v-html="event.title" />
+              <p v-if="event.acf" class="date">
+                {{ eventDate }}
               </p>
-              <p
-              v-if="event.acf"
-              class="location">
+              <p v-if="event.acf" class="location">
                 <a
-                :href='`https://maps.google.com/?q=${event.acf.location.address}`'
-                rel="no-openner"
-                target="_blank">
-                  <font-awesome-icon :icon="['fas', 'map-marked-alt']"/>
+                  v-if="event.acf.location.address"
+                  :href="
+                    `https://maps.google.com/?q=${event.acf.location.address}`
+                  "
+                  rel="no-openner"
+                  target="_blank"
+                >
+                  <font-awesome-icon :icon="['fas', 'map-marked-alt']" />
                   White Horse Tavern
                 </a>
               </p>
             </div>
-            <div
-            v-if="event.acf"
-            class="trivia-event__header__ctas">
+            <div v-if="event.acf" class="trivia-event__header__ctas">
               <a
-              :href="event.acf.googleCalendarLink"
-              class="button"
-              target="_blank"
-              rel="noopenner">Add To Google Calendar</a>
+                :href="event.acf.googleCalendarLink"
+                class="button"
+                target="_blank"
+                rel="noopenner"
+                >Add To Google Calendar</a
+              >
             </div>
           </div>
           <div class="grid-2of3 grid-1of1--notebook-large">
             <div
-            v-if="event.acf"
-            class="trivia-event__header__image background-image"
-            :style="{backgroundImage: `url(/triviaevents${event.acf.photoAsset})`}">
-            </div>
+              v-if="event.acf"
+              class="trivia-event__header__image background-image"
+              :style="{
+                backgroundImage: `url(/triviaevents${event.acf.photoAsset})`
+              }"
+            ></div>
           </div>
         </div>
       </header>
@@ -46,9 +48,10 @@
         <div class="container">
           <h2>About The Night</h2>
           <div
-          v-if="event.acf"
-          class="tiviaevent__content"
-          v-html="event.acf.description"/>
+            v-if="event.acf"
+            class="tiviaevent__content"
+            v-html="event.acf.description"
+          />
         </div>
       </section>
       <section class="section section--yellow trivia-event__rules">
@@ -59,7 +62,8 @@
               No phones during the rounds (no cheating!)
             </li>
             <li class="rules__rule">
-              Teams are limited to groups of six (teams larger than that are not eligible to win prizes, but can still play!)
+              Teams are limited to groups of six (teams larger than that are not
+              eligible to win prizes, but can still play!)
             </li>
             <li class="rules__rule">
               Answer sheets will be collected at the end of each round
@@ -68,13 +72,16 @@
               The answers have been researched and are set ahead of time
             </li>
             <li class="rules__rule">
-              Unless otherwise stated; voice work and cameos count for questions about actors
+              Unless otherwise stated; voice work and cameos count for questions
+              about actors
             </li>
             <li class="rules__rule">
-              Unless otherwise stated; box office stats are not adjusted for inflation and are domestic
+              Unless otherwise stated; box office stats are not adjusted for
+              inflation and are domestic
             </li>
             <li class="rules__rule">
-              Unless otherwise stated; Oscars round are regarding the release year of the film(s)
+              Unless otherwise stated; Oscars round are regarding the release
+              year of the film(s)
             </li>
           </ul>
         </div>
@@ -87,6 +94,7 @@
   query Post ($path: String!) {
     wordPressTriviaevent (path: $path) {
       title
+      slug
       acf {
         dateTime
         description
@@ -101,15 +109,15 @@
 </page-query>
 
 <style lang="scss">
-  @import "@/styles/templates/triviaevent.scss";
+@import "@/styles/templates/triviaevent.scss";
 </style>
 
 <script>
 export default {
-  name: 'TriviaEvent',
+  name: "TriviaEvent",
   data() {
     return {
-      event: {},
+      event: {}
     };
   },
   mounted() {
@@ -118,14 +126,35 @@ export default {
   computed: {
     eventDate() {
       const months = [
-        "January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
       ];
-      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const eventDate = new Date(this.event.acf.dateTime.replace(/\-/g, '/'));
-      const eventTime = `${(eventDate.getHours() + 11) % 12 + 1}:${eventDate.getMinutes()}`;
-      return `${days[eventDate.getDay()]}, ${months[eventDate.getMonth()]} ${eventDate.getDate()} at ${eventTime}pm`;
-    },
+      const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      const eventDate = new Date(this.event.acf.dateTime.replace(/\-/g, "/"));
+      const eventTime = `${((eventDate.getHours() + 11) % 12) +
+        1}:${eventDate.getMinutes()}`;
+      return `${days[eventDate.getDay()]}, ${
+        months[eventDate.getMonth()]
+      } ${eventDate.getDate()} at ${eventTime}pm`;
+    }
   },
   metaInfo() {
     return {
@@ -133,37 +162,39 @@ export default {
       meta: [
         {
           property: "og:title",
-          content: `Marquee Trivia - ${this.$page.wordPressTriviaevent.title}`,
+          content: `Marquee Trivia - ${this.$page.wordPressTriviaevent.title}`
         },
         {
           property: "og:description",
-          content: "Boston’s first trivia night for film lovers. Every month we dive into movie marginalia, Oscars history, box office stats, and more!",
+          content:
+            "Boston’s first trivia night for film lovers. Every month we dive into movie marginalia, Oscars history, box office stats, and more!"
         },
         {
           property: "og:url",
-          content: `https://www.marqueetrivia.com${this.$route.path}`,
+          content: `https://www.marqueetrivia.com${this.$route.path}`
         },
         {
           property: "og:image",
-          content: `https://www.marqueetrivia.com/triviaevents${this.$page.wordPressTriviaevent.acf.photoAsset}`,
+          content: `https://www.marqueetrivia.com/triviaevents${this.$page.wordPressTriviaevent.acf.photoAsset}`
         },
         {
           property: "twitter:title",
-          content: "Marquee Trivia",
+          content: "Marquee Trivia"
         },
         {
           property: "twitter:description",
-          content: "Boston’s first trivia night for film lovers. Every month we dive into movie marginalia, Oscars history, box office stats, and more!",
+          content:
+            "Boston’s first trivia night for film lovers. Every month we dive into movie marginalia, Oscars history, box office stats, and more!"
         },
         {
           property: "twitter:card",
-          content: "summary_large_image",
+          content: "summary_large_image"
         },
         {
           property: "twitter:image",
-          content: `https://www.marqueetrivia.com/triviaevents${this.$page.wordPressTriviaevent.acf.photoAsset}`,
+          content: `https://www.marqueetrivia.com/triviaevents${this.$page.wordPressTriviaevent.acf.photoAsset}`
         }
-      ],
+      ]
     };
   }
 };
